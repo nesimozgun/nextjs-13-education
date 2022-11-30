d02e7c96b2eba08373e0dfacf6cc0024c71f89d3
U180709044 <ozgunnesim@gmail.com>
2022-11-30 20:47:14 +0300

    first commit

---
+++ app/[id]/not-found.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <div>
+            <center><h1>Blog does not exist.</h1></center>
+        </div>
+    );
+}
